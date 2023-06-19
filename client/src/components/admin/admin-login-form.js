bf76615ac114b3adc9b6938e7a93d385a1e27151
Wilantara Jaya <wilantarajaya.123@gmail.com>
2023-06-19 23:25:08 +0800

    styling button

--- client/src/components/admin/admin-login-form.js
+++ client/src/components/admin/admin-login-form.js
@@ -4,7 +4,7 @@ import { ContentMiddle } from "../../styles/shared-styles";
 
 export const AdminLoginForm = () => {
   const theme = useTheme();
-  const lightColor = theme.palette.light.main
+  const lightColor = theme.palette.light.main;
   return (
     <Grid
       container
@@ -12,22 +12,31 @@ export const AdminLoginForm = () => {
         width: "970px",
         height: "569px",
         boxShadow: "0px 10px 15px 3px rgba(226, 226, 226, 0.25)",
-        borderRadius: '10px', 
-        overflow: 'hidden'
+        borderRadius: "10px",
+        overflow: "hidden",
       }}
     >
-      <Grid
-        item
-        xs={6}
-        sx={{ ...ContentMiddle, backgroundColor: lightColor }}
-      >
+      <Grid item xs={6} sx={{ ...ContentMiddle, backgroundColor: lightColor }}>
         <Box>
           <Typography variant="h4" component="h4">
             Admin Login
           </Typography>
         </Box>
-          <Button variant="secondary" size="small">Masuk</Button>
-          <Button variant="primary" size="small">Masuk</Button>
+        {/* <Button variant="secondary" size="small">Masuk</Button> */}
+
+        <Button
+          variant="contained"
+          size="small"
+          sx={{
+            color: "#fffff",
+            backgroundColor: "#0E404F",
+            textTransform: "none",
+            borderRadius: "10px",
+            padding: "20px 0px",
+          }}
+        >
+          Login
+        </Button>
       </Grid>
       <Grid
         item
