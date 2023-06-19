adcba60a04488ad97cdb5793d450c8e356f4b9e8
Renrensan <pasha.renaisan@gmail.com>
2023-06-19 15:35:02 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { AdminLoginPage } from './pages/admin/admin-login-page';
+
+function App() {
+  return (
+    <>
+    <AdminLoginPage/>
+    </>
+  );
+}
+
+export default App;
