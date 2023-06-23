93c4aa793c3f801db5c42b87459bc257b0069724
razerblaze <ftapamahendra@gmail.com>
2023-06-23 16:40:28 +0800

    feat(main): add code to the main file of the program

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const router = require('./routes/v1/route.js');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false }));
+
+
+app.use('/api', router);
+
+app.listen(3000)
