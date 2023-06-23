656304c09223100fc0efebd3e195df0a0bcd422e
razerblaze <ftapamahendra@gmail.com>
2023-06-23 16:20:13 +0800

    refactor(connect-database): using sequelize as base to connect to database

--- server/app/connect-database.js
+++ server/app/connect-database.js
@@ -1,10 +1,18 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "nusawita",
-    password: "azerblaze195",
-    port: 5432
-})
+const Sequelize = require('sequelize');
 
-module.exports = pool
+// Create a Sequelize instance and establish the database connection
+const sequelize = new Sequelize('latihan_6', 'postgres', 'azerblaze195', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+//check connection to database
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
+module.exports = sequelize
