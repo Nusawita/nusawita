ee80227214a6ed2746d77833103b04d23ef636f2
razerblaze <ftapamahendra@gmail.com>
2023-06-25 00:46:19 +0800

    refactor(server): add .env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
 const router = require('./app/routes/v1/route.js');
+
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
@@ -10,4 +15,6 @@ app.use(bodyParser.urlencoded({ extended : false }));
 
 app.use('/api', router);
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
