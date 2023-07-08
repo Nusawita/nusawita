28b481da13fd5a31d6be61efcd090db307812a6b
razerblaze <ftapamahendra@gmail.com>
2023-07-08 22:57:38 +0800

    add allwed header to cors

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(cors({
     origin: 'http://localhost:3000',
     methods: ["GET", "POST"],
     credentials:true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }));
 
 app.use(bodyParser.json())
