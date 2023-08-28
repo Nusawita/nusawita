b563eaa2f747952635f0aae63fec3a65d777b094
Renrensan <pasha.renaisan@gmail.com>
2023-08-28 13:31:20 +0800

    chores(restructure): Remove unnecesary file 1. Remove server folder

--- server/app/docs/servers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    servers: [
-        {
-            url: "http://localhost:3000/api",
-            description: "Local server",
-        },
-    ],
-}
--- server/app/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const UserController = require('../controllers/userController');
-const UserRepository = require('../repositories/userRepository');
-const UserService = require('../services/userService');
-
-const userRepository = new UserRepository();
-const userService = new UserService(userRepository);
-exports.userController = new UserController(userService);
